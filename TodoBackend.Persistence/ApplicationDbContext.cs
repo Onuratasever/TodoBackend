746ef5be08eb07c91fb1779d0987bf3d5e833a6f
Onuratasever <onuratasever24@gmail.com>
2024-09-20 18:34:52 +0300

    ApplicationDbContext is created and dbsets are created. Model's configuration files' assembly is used for model creation.

---
+++ TodoBackend.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using TodoBackend.Domain.Entities.Auth;
+using TodoBackend.Domain.Entities.Comment;
+using TodoBackend.Domain.Entities.SubTask;
+using TodoBackend.Domain.Entities.TodoItem;
+using TodoBackend.Domain.Entities.TodoList;
+using TodoBackend.Domain.Entities.User;
+
+namespace TodoBackend.Persistence;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions options): base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Auth> Auths { get; set; }
+    public DbSet<TodoList> TodoLists { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<SubTask> SubTasks { get; set; }
+    public DbSet<TodoItem> TodoItems { get; set; }
+    
+}
