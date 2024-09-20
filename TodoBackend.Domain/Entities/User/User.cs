525e8089e29ee82a139358aca3de7e700518fb22
Onuratasever <onuratasever24@gmail.com>
2024-09-20 18:33:43 +0300

    First name and last name properties are added.

--- TodoBackend.Domain/Entities/User/User.cs
+++ TodoBackend.Domain/Entities/User/User.cs
@@ -4,6 +4,8 @@ namespace TodoBackend.Domain.Entities.User
 {
     public class User: BaseEntity
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public DateTime CreatedAt { get; set; }
