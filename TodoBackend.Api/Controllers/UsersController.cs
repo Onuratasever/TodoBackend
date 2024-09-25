b261a18f2b3d0e8af1c4c7383ec8b6de06b9ca18
Onuratasever <onuratasever24@gmail.com>
2024-09-25 23:58:00 +0300

    User controller implementation

--- TodoBackend.Api/Controllers/UsersController.cs
+++ TodoBackend.Api/Controllers/UsersController.cs
@@ -14,21 +14,75 @@ public class UsersController: ControllerBase
         _userRepository = userRepository;
     }
     
-    [HttpGet]
-    public async Task Get()
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetUserById(Guid id)
     {
-         _userRepository.Add(new()
-            {
-                Id = Guid.NewGuid(),
-                FirstName = "John",
-                LastName = "Doe",
-                Username = "johndoe",
-                Email = "email",
-                CreatedAt = DateTime.Now,//.ToString("MM/dd/yyyy hh:mm:ss tt", CultureInfo.InvariantCulture),
-                Role = "Admin",
-                UpdatedAt = DateTime.Now
-            }
-        );
+        var user = await _userRepository.GetByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(user);
+    }
+    
+    [HttpGet("getAllUsers")]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var users = await _userRepository.GetAllAsync();
+        return Ok(users);
+    }
+    
+    [HttpPost("createUser")]
+    public async Task<IActionResult> CreateUser(User user)
+    {
+        if(user == null)
+        {
+            return BadRequest();
+        }
+        user.Id = Guid.NewGuid();
+        user.CreatedAt = DateTime.Now;
+        user.UpdatedAt = DateTime.Now;
+        
+        _userRepository.Add(user);
+        
         await _userRepository.SaveChangesAsync();
+        return Ok(user);
+    }
+    
+    [HttpPut("updateUser/{id:guid}")]
+    public async Task<IActionResult> UpdateUser(Guid id, User user) //userden önce frombody
+    {
+        var existingUser = await _userRepository.GetByIdAsync(id);
+        if(existingUser == null)
+        {
+            return NotFound("User not found");
+        }
+        
+        existingUser.FirstName = user.FirstName;
+        existingUser.LastName = user.LastName;
+        existingUser.Username = user.Username;
+        existingUser.Email = user.Email;
+        existingUser.Role = user.Role;
+        existingUser.UpdatedAt = DateTime.Now;
+        
+        _userRepository.Update(existingUser);
+        
+        await _userRepository.SaveChangesAsync();
+        return Ok(existingUser);
+    }
+    
+    [HttpDelete("deleteUser/{id:guid}")]
+    public async Task<IActionResult> DeleteUser(Guid id)
+    {
+        var existingUser = await _userRepository.GetByIdAsync(id);
+        if(existingUser == null)
+        {
+            return NotFound("User not found");
+        }
+        
+        _userRepository.Delete(existingUser);
+        
+        await _userRepository.SaveChangesAsync();
+        return Ok();
     }
 }
