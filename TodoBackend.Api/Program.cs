3dc79b246fc70f8f5b148745b6808d23447fbbcb
Onuratasever <onuratasever24@gmail.com>
2024-09-26 00:01:26 +0300

    Cors policy and extension method called.

--- TodoBackend.Api/Program.cs
+++ TodoBackend.Api/Program.cs
@@ -1,8 +1,14 @@
 using TodoBackend.Persistence;
+using TodoBackend.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistenceServices(builder.Configuration); //This parameter will be passed to the AddPersistenceServices function
+builder.Services.AddApplication(); //This function will add the application services to the DI container
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+//todo change this cors later
+
 builder.Services.AddControllers(); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,5 +25,6 @@ else
     app.UseHttpsRedirection();
 }
 
+app.UseCors();
 app.MapControllers(); //This function will add the routing for the controllers 
 app.Run();
