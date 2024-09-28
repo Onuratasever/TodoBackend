8dec57b82001ce28cce64fb943d65a8b8557f0d1
Onuratasever <onuratasever24@gmail.com>
2024-09-28 19:42:01 +0300

    Rename files with incorrect names

--- TodoBackend.Application/Users/GetAll/GetAllQueryRequest.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-
-namespace TodoBackend.Application.Users.GetAll;
-
-public class GetAllQueryRequest : IRequest<GetAllQueryResponse>
-{
-    
-}
---
+++ TodoBackend.Application/Users/GetAll/GetAllUsersQueryRequest.cs
@@ -0,0 +1,8 @@
+using MediatR;
+
+namespace TodoBackend.Application.Users.GetAll;
+
+public class GetAllUsersQueryRequest : IRequest<GetAllUsersQueryResponse>
+{
+    
+}
