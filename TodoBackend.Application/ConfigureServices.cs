ab749a851ee56b93de412486416c68fd6cab9495
Onuratasever <onuratasever24@gmail.com>
2024-09-28 19:39:55 +0300

    Configure FluentValidation automatic validation - Added FluentValidationAutoValidation() to handle automatic validation - Registered validators from AssemblyReference.Application

--- TodoBackend.Application/ConfigureServices.cs
+++ TodoBackend.Application/ConfigureServices.cs
@@ -1,5 +1,8 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using TodoBackend.Application.Users.Create;
 
 namespace TodoBackend.Application;
 
@@ -7,10 +10,14 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddFluentValidationAutoValidation();
+        services.AddValidatorsFromAssembly(AssemblyReference.Application);
         services.AddMediatR(configuration =>
         {
             configuration.RegisterServicesFromAssembly(AssemblyReference.Application); // değiştir
         });
+        // services.AddFluentValidation(configuration =>
+        //     configuration.RegisterValidatorsFromAssemblyContaining<AssemblyReference.Application>());
         return services; // değiştir
     }
 
